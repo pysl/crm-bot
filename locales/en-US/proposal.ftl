2f40f7dcdd7515204308836bdd9036658ee0ddf9
John William Bowen <johnwb1776@gmail.com>
2023-05-28 10:07:03 -0400

    update to use new command formate

---
+++ locales/en-US/proposal.ftl
@@ -0,0 +1,6 @@
+command-name = proposal
+command-description = Create, view, update, and manage proposals
+
+# Start command
+start-name = start
+start-description = Create a new proposal
---
+++ src/commands/chat/execution/proposal/index.ts
@@ -0,0 +1,6 @@
+import { ChatInputCommandInteraction } from "discord.js";
+import { ns } from '../../builders/proposal';
+
+export async function proposal(interaction: ChatInputCommandInteraction<'cached'>) {
+	const { locale } = interaction
+}
